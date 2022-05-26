c7031c8e9e2ed8da44f92807f4e7ef72e552f3c3
Jack Kelly <jack@jackkelly.name>
2022-05-27 06:49:22 +1000

    class CommutativeProduct: haddock

--- src/TEMP.hs
+++ src/TEMP.hs
@@ -6,6 +6,23 @@ module TEMP
 import Data.Int
 import Data.Word
 
+-- | Subclass of 'Num' where '(*)' is commutative.
+--
+-- 'Num' doesn't demand commutative '(*)', and there are reasonable
+-- "real-world" instances with non-commutative multiplication. There
+-- is also no canonical subclass in @base@ that would suffice, as both
+-- 'Integral' and 'Floating' imply commutative '(*)' for different
+-- reasons.
+--
+-- Two examples of non-commutative '(*)':
+--
+-- * @Linear.Quaternion.Quaterion@ from the @linear@ package has a
+--   'Num' instance, and quaternion multiplication is
+--   noncommutative.
+--
+-- * @Data.Matrix.Matrix@ from the @matrix@ package uses '(*)' for
+--   matrix multiplication, which is also non-commutative (on square
+--   matrices, which is the only time the question makes sense).
 class Num a => CommutativeProduct a
 
 instance CommutativeProduct Int8
